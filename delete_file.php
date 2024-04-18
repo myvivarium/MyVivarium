05d589e6adabfc62ef82a4804c37daccc1c69b53
myvivarium <myvivarium.online@gmail.com>
2024-04-18 19:57:32 -0400

    Update delete_file.php

--- delete_file.php
+++ delete_file.php
@@ -6,11 +6,12 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $fileQuery = "SELECT * FROM files WHERE id = '$id'";
     $file = $con->query($fileQuery)->fetch_assoc();
+    $cage_id = $file['cage_id'];
     if (file_exists($file['file_path'])) {
         unlink($file['file_path']); // Delete the file from the server
         $delete = "DELETE FROM files WHERE id = '$id'";
         $con->query($delete);
-        $url = urldecode($_GET['url']).".php?id=".$id;
+        $url = urldecode($_GET['url']).".php?id=".$cage_id;
         header("Location: $url");
     }
 }
