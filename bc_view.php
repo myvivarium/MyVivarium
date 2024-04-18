0db47b0576ad9e1fa3a53fa098057b09fb5fa216
myvivarium <myvivarium.online@gmail.com>
2024-04-18 13:26:23 -0400

    Update bc_view.php

--- bc_view.php
+++ bc_view.php
@@ -43,8 +43,6 @@ require 'header.php';
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <!-- Bootstrap JS for Dropdown -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <title>View Breeding Cage | <?php echo htmlspecialchars($labName); ?></title>
 
