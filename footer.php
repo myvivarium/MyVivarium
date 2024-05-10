b1b936eccbb1ff6fa8a656c7f1a59dd6ba7e6bd3
myvivarium <myvivarium.online@gmail.com>
2024-05-10 17:15:42 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
 <!-- Footer Section -->
+
+<style>
+    /* Header and Footer Styling */
+    .header-footer {
+            background-color: #343a40;
+            padding: 20px 0;
+            text-align: center;
+            width: 100%;
+            box-sizing: border-box;
+        }
+</style>
 <div class="header-footer">
     <p style="color:white">&copy;
         <?php echo date("Y"); ?> <?php echo htmlspecialchars($labName); ?>. All rights reserved.
