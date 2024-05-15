9d7204159758391626b7420412f4979f8b407be5
myvivarium <myvivarium.online@gmail.com>
2024-05-15 13:21:13 -0400

    Update hc_slct_crd.php

--- hc_slct_crd.php
+++ hc_slct_crd.php
@@ -93,7 +93,7 @@ require 'header.php';
             $('#cageIds').select2({
                 placeholder: "Select Cage IDs",
                 allowClear: true,
-                width: '100%'
+                width: '50%'
             });
         });
     </script>
