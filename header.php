9f0f6d00b9966d1591a7ddc63c47b036bb673b4d
myvivarium <myvivarium.online@gmail.com>
2024-05-22 18:15:01 -0400

    Update header.php

--- header.php
+++ header.php
@@ -26,6 +26,10 @@ if ($row = mysqli_fetch_assoc($labResult)) {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <!-- Bootstrap JS for Dropdown -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 
     <style>
         /* General Styles */
