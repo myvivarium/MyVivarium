171a45683efd38efae7713a4225915ea55b3fc1a
myvivarium <myvivarium.online@gmail.com>
2024-05-28 20:03:52 -0400

    Update header.php

--- header.php
+++ header.php
@@ -36,9 +36,9 @@ if ($row = mysqli_fetch_assoc($labResult)) {
 
     <!-- Google Font: Poppins -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-
-    <!-- Bootstrap JS for interactive components -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap JS for Dropdown -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <style>
         body {
