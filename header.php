99387730e934b2b2b945dc89887c0830307a6638
myvivarium <myvivarium.online@gmail.com>
2024-05-29 15:41:32 -0400

    Update header.php

--- header.php
+++ header.php
@@ -40,7 +40,6 @@ if ($row = mysqli_fetch_assoc($labResult)) {
         body {
             margin: 0;
             padding: 0;
-            font-family: 'Poppins', sans-serif;
         }
 
         .header-footer {
