a32a35236ac4377426a5731b0fe57f859385d9dd
myvivarium <myvivarium.online@gmail.com>
2024-05-28 20:06:07 -0400

    changes

--- header.php
+++ header.php
@@ -37,9 +37,6 @@ if ($row = mysqli_fetch_assoc($labResult)) {
     <!-- Google Font: Poppins -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     
-    <!-- Bootstrap JS for Dropdown -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
     <style>
         body {
             margin: 0;
@@ -141,6 +138,10 @@ if ($row = mysqli_fetch_assoc($labResult)) {
             <a href="logout.php" class="btn btn-secondary">Logout</a>
         </nav>
     </div>
+
+    <!-- Bootstrap and jQuery JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
