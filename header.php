7fafeb55ec41a0b173fbb1a050e82a113df2a2d9
myvivarium <myvivarium.online@gmail.com>
2024-05-30 15:20:59 -0400

    manifest

--- header.php
+++ header.php
@@ -19,13 +19,13 @@ if ($row = mysqli_fetch_assoc($labResult)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Favicon and icons for different devices -->
-    <link rel="icon" href="/icons/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-    <link rel="icon" sizes="192x192" href="/icons/android-chrome-192x192.png">
-    <link rel="icon" sizes="512x512" href="/icons/android-chrome-512x512.png">
-    <link rel="manifest" href="/icons/site.webmanifest">
+    <link rel="icon" href="./icons/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="./icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./icons/favicon-16x16.png">
+    <link rel="icon" sizes="192x192" href="./icons/android-chrome-192x192.png">
+    <link rel="icon" sizes="512x512" href="./icons/android-chrome-512x512.png">
+    <link rel="manifest" href="./icons/manifest.json">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
