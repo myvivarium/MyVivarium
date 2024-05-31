f4c9a2501d2e20275d8ad17ae0ed0bb0be50d2c8
myvivarium <myvivarium.online@gmail.com>
2024-05-31 11:18:39 -0400

    Update bc_edit.php

--- bc_edit.php
+++ bc_edit.php
@@ -210,8 +210,6 @@ require 'header.php';
 
                             </div>
 
-                            <?php include 'bcltr_dash.php'; ?>
-
                             <!-- Separator -->
                             <hr class="mt-4 mb-4" style="border-top: 3px solid #000;">
 
@@ -270,6 +268,9 @@ require 'header.php';
 
                         </form>
                     </div>
+
+                    <?php include 'bcltr_dash.php'; ?>
+                    
                 </div>
             </div>
         </div>
