1acf69940bf6c21fa1d2e21985a52f8fa13e43e9
myvivarium <myvivarium.online@gmail.com>
2024-05-31 21:57:39 -0400

    Update iot_sensors.php

--- iot_sensors.php
+++ iot_sensors.php
@@ -38,6 +38,9 @@ require 'header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>IOT Sensors | <?php echo htmlspecialchars($labName); ?></title>
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!-- Inline CSS for styling -->
     <style>
         body {
@@ -48,70 +51,61 @@ require 'header.php';
         .iframe-container {
             position: relative;
             width: 100%;
-            height: 800px;
+            height: auto;
+            margin-top: 20px;
         }
 
-        .iframe-top-left,
-        .iframe-top-right,
-        .iframe-bottom-left,
-        .iframe-bottom-right {
-            position: absolute;
-            width: 50%;
-            height: 50%;
+        .iframe {
+            width: 100%;
+            height: 300px;
             border: none;
-            margin: 50px 0px;
-        }
-
-        .iframe-top-left {
-            top: 0;
-            left: 0;
-        }
-
-        .iframe-top-right {
-            top: 0;
-            right: 0;
-        }
-
-        .iframe-bottom-left {
-            bottom: 0;
-            left: 0;
-        }
-
-        .iframe-bottom-right {
-            bottom: 0;
-            right: 0;
         }
     </style>
 </head>
 
 <body>
-
-    <div class="container">
+    <div class="container mt-4">
         <!-- Section for Room 1 IOT Sensors -->
         <?php if (!empty($r1_temp) || !empty($r1_humi) || !empty($r1_illu) || !empty($r1_pres)) : ?>
-            <div style="margin-top: 50px;">
-                <h2><?php echo htmlspecialchars($labName); ?> - IOT Sensors</h2>
-            </div>
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h2><?php echo htmlspecialchars($labName); ?> - Room 1 IOT Sensors</h2>
+                </div>
 
-            <div class="iframe-container">
-                <iframe class="iframe-top-left" src="<?php echo htmlspecialchars($r1_temp); ?>" width="450" height="300" frameborder="0"></iframe>
-                <iframe class="iframe-top-right" src="<?php echo htmlspecialchars($r1_humi); ?>" width="450" height="300" frameborder="0"></iframe>
-                <iframe class="iframe-bottom-left" src="<?php echo htmlspecialchars($r1_illu); ?>" width="450" height="300" frameborder="0"></iframe>
-                <iframe class="iframe-bottom-right" src="<?php echo htmlspecialchars($r1_pres); ?>" width="450" height="300" frameborder="0"></iframe>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r1_temp); ?>"></iframe>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r1_humi); ?>"></iframe>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r1_illu); ?>"></iframe>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r1_pres); ?>"></iframe>
+                </div>
             </div>
         <?php endif; ?>
 
         <!-- Section for Room 2 IOT Sensors -->
         <?php if (!empty($r2_temp) || !empty($r2_humi) || !empty($r2_illu) || !empty($r2_pres)) : ?>
-            <div style="margin-top: 50px;">
-                <h2><?php echo htmlspecialchars($labName); ?> - Room 2 IOT Sensors</h2>
-            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h2><?php echo htmlspecialchars($labName); ?> - Room 2 IOT Sensors</h2>
+                </div>
 
-            <div class="iframe-container">
-                <iframe class="iframe-top-left" src="<?php echo htmlspecialchars($r2_temp); ?>" width="450" height="300" frameborder="0"></iframe>
-                <iframe class="iframe-top-right" src="<?php echo htmlspecialchars($r2_humi); ?>" width="450" height="300" frameborder="0"></iframe>
-                <iframe class="iframe-bottom-left" src="<?php echo htmlspecialchars($r2_illu); ?>" width="450" height="300" frameborder="0"></iframe>
-                <iframe class="iframe-bottom-right" src="<?php echo htmlspecialchars($r2_pres); ?>" width="450" height="300" frameborder="0"></iframe>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r2_temp); ?>"></iframe>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r2_humi); ?>"></iframe>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r2_illu); ?>"></iframe>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <iframe class="iframe" src="<?php echo htmlspecialchars($r2_pres); ?>"></iframe>
+                </div>
             </div>
         <?php endif; ?>
     </div>
@@ -119,6 +113,8 @@ require 'header.php';
     <!-- Include footer -->
     <?php include 'footer.php'; ?>
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
