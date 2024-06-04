36b4e7b12a1b460989366b2a585def595a2f1644
myvivarium <myvivarium.online@gmail.com>
2024-06-04 09:09:17 -0400

    changes

--- hc_edit.php
+++ hc_edit.php
@@ -267,6 +267,8 @@ require 'header.php';
                     <div class="card-body">
                         <form method="POST" action="hc_edit.php?id=<?= $id; ?>" enctype="multipart/form-data">
 
+                            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+
                             <div class="mb-3">
                                 <label for="cage_id" class="form-label">Cage ID</label>
                                 <input type="text" class="form-control" id="cage_id" name="cage_id" value="<?= htmlspecialchars($holdingcage['cage_id']); ?>" required>
