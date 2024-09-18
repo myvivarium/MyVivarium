06cf7f879c2a8ec7527dd6e315c4e7c9f5cf0ce4
Robinson Vidva <robinson.vidva@gmail.com>
2024-09-18 09:32:45 -0400

    Update manage_reminder.php

--- manage_reminder.php
+++ manage_reminder.php
@@ -96,7 +96,7 @@ $reminderResult = $con->query($reminderQuery);
     <!-- Include necessary styles and scripts -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <style>
 
         /* Popup and Overlay Styles */
