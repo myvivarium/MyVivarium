56478bc6147c24b0b03497f6bafa20e1df4ffd22
Robinson Vidva <robinson.vidva@gmail.com>
2024-09-17 21:06:28 -0400

    update

--- manage_reminder.php
+++ manage_reminder.php
@@ -466,6 +466,8 @@ $reminderResult = $con->query($reminderQuery);
         </div>
     </div>
 
+    <!-- Include necessary scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
     <script>
         // JavaScript code to handle form interactions
